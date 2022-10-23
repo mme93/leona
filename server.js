61a280ff22ff02248a32b55fe75fb76d28ad350f
magantus@googlemail.com <Extrem93!>
2022-10-23 22:51:50 +0200

    update

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-const express = require("express");
-const path = require("path");
-
+const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + "/www"));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "www", "index.html"));
-});
-
-app.listen(process.env.PORT || 8100);
+app.use(express.static(__dirname + '/dist/<app-name>'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));});
+app.listen(process.env.PORT || 8080);
